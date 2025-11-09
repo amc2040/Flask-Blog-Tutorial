5f4e08b9aea57ce0831b48d5c89f469123415793
Amber Carr <ambercarr@Ambers-MacBook-Pro-2.local>
2025-11-08 22:52:35 -0600

    created route to delete post

--- app.py
+++ app.py
@@ -106,5 +106,27 @@ def edit(id):
     #If GET then display page
     return render_template('edit.html', post=post)
 
+#create a route to delete a post
+#delete a post will only be processed with a POST method
+#the post id is the url parameter
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    #get the post
+    post = get_post(id)
 
-app.run(port=5008)
+    #connect to the database
+    conn =get_db_connection()
+
+    #execute a delete query 
+    conn.execute('DELETE from posts WHERE id= ?', (id))
+    #commit and close the connection
+    conn.commit()
+    conn.close()
+
+    #flash a success message 
+    flash('"{}" was successfully deleted!'.format(post['title']))
+
+    #redirect to the homepage
+    return redirect(url_for('index'))
+
+    app.run(port=5008)
